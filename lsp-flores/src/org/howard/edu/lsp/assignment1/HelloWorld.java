73440c1d20d2bd98b88e77dd03e4044993dc2245
Flores314 <floresja314@gmail.com>
2022-08-31 21:52:21 -0400

    This is my commit for assignment 1.

---
+++ lsp-flores/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,14 @@
+package org.howard.edu.lsp.assignment1;
+/**
+ * Jonathan Flores
+ * @author jonhi
+ *
+ */
+public class HelloWorld {
+	
+	public static void main(String args[]) {
+
+        System.out.println("Hello World");
+    }
+
+}
